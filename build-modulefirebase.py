6f707edcb1371b5a1e434d5f80e1c372e0d70a37
ryantangmj <110431837+ryantangmj@users.noreply.github.com>
2023-07-05 22:56:51 +0800

    connected modules from NUS mods to ChatPage

--- build-modulefirebase.py
+++ build-modulefirebase.py
@@ -4,7 +4,6 @@ import re
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate('./the-unifiers-firebase-adminsdk-vzs1p-06f0a83999.json')  # Replace with your actual JSON file path
 firebase_admin.initialize_app(cred)
@@ -14,12 +13,13 @@ with open("moduleList.json", "r") as file:
     data = json.load(file)
 
 pattern = re.compile(r'^CS[0-9]|^IS[0-9]|^BT[0-9]', re.IGNORECASE)
-mods_list = []
 
 for module in data:
     module_code = module["moduleCode"]
     if pattern.match(module_code):
-        mods_list.append(module_code)
-        
-doc_ref = db.collection("modules").document("module_list")
-doc_ref.set({"modules": mods_list})
+        doc_ref = db.collection('chats').document()
+        module_data = {
+            "moduleCode": module_code,
+            "messages": []  # Add an empty "messages" field to the module
+        }
+        doc_ref.set(module_data)
