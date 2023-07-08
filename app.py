d93e1b05376960de252c15aceecb199db5c34f9b
tallkoh <xnkohs@gmail.com>
2023-07-08 17:05:56 +0800

    Update POST method

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import subprocess
 import asyncio
+import os
 from aiohttp import web
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
@@ -18,70 +19,65 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 async def run_subprocess(channel_username):
-    process = await asyncio.create_subprocess_exec(
-        'python', 'runfirestore.py', '--telegram-channel', channel_username,
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
+	process = await asyncio.create_subprocess_exec(
+		'python', 'runfirestore.py', '--telegram-channel', channel_username,
+		stdout=subprocess.PIPE, stderr=subprocess.PIPE
+	)
 
-    stdout, stderr = await process.communicate()
-    
-    
+	stdout, stderr = await process.communicate()
+	
 async def run_subprocess_put(channel_username, min_id):
-    process = await asyncio.create_subprocess_exec(
-        'python', 'runfirestore.py', '--telegram-channel', channel_username, '--min-id', min_id,
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
+	process = await asyncio.create_subprocess_exec(
+		'python', 'runfirestore.py', '--telegram-channel', channel_username, '--min-id', min_id,
+		stdout=subprocess.PIPE, stderr=subprocess.PIPE
+	)
 
-    stdout, stderr = await process.communicate()
+	stdout, stderr = await process.communicate()
 
-class Scraping(Resource):
-    @app.route('/channels/<channel_username>', methods=['POST'])
-    def add_channel(channel_username):
-        # channel_name = request.form['channel_name']
-        
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(run_subprocess(channel_username))
-        return jsonify({'message': 'Channel added successfully'})
-    
-    @app.route('/channels/<channel_username>', methods=['PUT'])
-    def update_channel(channel_username):
-        try:
-            # channel_name = request.form['channel_name']
+@app.route('/channels/<channel_username>', methods=['POST'])
+def add_channel(channel_username):
+	# channel_name = request.form['channel_name']
+	print("you got here")
+	loop = asyncio.new_event_loop()
+	asyncio.set_event_loop(loop)
+	loop.run_until_complete(run_subprocess(channel_username))
+	return jsonify({'message': 'Channel added successfully'})
+	
+@app.route('/channels/<channel_username>', methods=['PUT'])
+def update_channel(channel_username):
+	try:
+		# channel_name = request.form['channel_name']
+		# Query Firestore to get the latest message for the channel
+		messages_ref = db.collection('news').where('channel_username', '==', channel_username).order_by('message_id', direction='desc').limit(1)
+		query = messages_ref.get()
 
-            # Query Firestore to get the latest message for the channel
-            messages_ref = db.collection('news').where('channel_username', '==', channel_username).order_by('message_id', direction='desc').limit(1)
-            query = messages_ref.get()
+		last_message_id = None
 
-            last_message_id = None
+		# Check if there is a message for the channel
+		if not query.empty:
+			last_message = query.docs[0]
+			last_message_id = last_message.get('message_id')
 
-            # Check if there is a message for the channel
-            if not query.empty:
-                last_message = query.docs[0]
-                last_message_id = last_message.get('message_id')
+		loop = asyncio.new_event_loop()
+		asyncio.set_event_loop(loop)
+		loop.run_until_complete(run_subprocess(channel_username, last_message_id))
+		return jsonify({'success': True, 'message': 'Channel updated successfully'})
 
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            loop.run_until_complete(run_subprocess(channel_username, last_message_id))
-            return jsonify({'success': True, 'message': 'Channel updated successfully'})
+	except Exception as e:
+		return jsonify({'success': False, 'error': str(e)})    
 
-        except Exception as e:
-            return jsonify({'success': False, 'error': str(e)})    
+	# return jsonify({'message': f'Channel with ID {channel_id} updated successfully'})
+	
+@app.route('/channels', methods=['GET'])
+def get_scraping(channel_username):
+	return jsonify({'message': 'Scraping updated successfully'})
+	
+@app.route('/channels/<channel_username>', methods=['DELETE'])
+def delete_channel(channel_username):
+	# Code to delete the channel with the given channel_id
+	# ...
 
-        # return jsonify({'message': f'Channel with ID {channel_id} updated successfully'})
-    
-    @app.route('/channels', methods=['GET'])
-    def get_scraping(channel_username):
-        return jsonify({'message': 'Scraping updated successfully'})
-    
-    @app.route('/channels/<channel_username>', methods=['DELETE'])
-    def delete_channel(channel_username):
-        # Code to delete the channel with the given channel_id
-        # ...
-
-        return jsonify({'message': f'Channel with ID {channel_id} deleted successfully'})
-    
-api.add_resource(Scraping, '/scraping/<string:channel_username>')
-
+	return jsonify({'message': f'Channel with ID {channel_id} deleted successfully'})
+	
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
